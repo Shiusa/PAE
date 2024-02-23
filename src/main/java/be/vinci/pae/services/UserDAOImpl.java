9102aca9bbc6ea520d8c5c01632e2999f7b16200
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-02-23 14:33:21 +0100

    add getOneUserByEmail UserDAO

--- src/main/java/be/vinci/pae/services/UserDAOImpl.java
+++ src/main/java/be/vinci/pae/services/UserDAOImpl.java
@@ -1,7 +1,12 @@
 package be.vinci.pae.services;
 
+import be.vinci.pae.domain.dto.UserDTO;
 import be.vinci.pae.services.dao.UserDAO;
 
 public class UserDAOImpl implements UserDAO {
 
+  @Override
+  public UserDTO getOneUserByEmail(String email) {
+    return null;
+  }
 }
--- src/main/java/be/vinci/pae/services/dao/UserDAO.java
+++ src/main/java/be/vinci/pae/services/dao/UserDAO.java
@@ -1,5 +1,9 @@
 package be.vinci.pae.services.dao;
 
+import be.vinci.pae.domain.dto.UserDTO;
+
 public interface UserDAO {
+
+  UserDTO getOneUserByEmail(String email);
 
 }
