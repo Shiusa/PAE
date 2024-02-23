c38ecc7e82bcaecf3ae8cb5fb68befa4bef91ad4
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-02-23 15:48:14 +0100

    refactor: dalService binding

--- src/main/java/be/vinci/pae/utils/ApplicationBinder.java
+++ src/main/java/be/vinci/pae/utils/ApplicationBinder.java
@@ -4,6 +4,8 @@ import be.vinci.pae.domain.User;
 import be.vinci.pae.domain.UserImpl;
 import be.vinci.pae.services.UserDAOImpl;
 import be.vinci.pae.services.dao.UserDAO;
+import be.vinci.pae.services.utils.DalService;
+import be.vinci.pae.services.utils.DalServiceImpl;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.ext.Provider;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
@@ -15,5 +17,6 @@ public class ApplicationBinder extends AbstractBinder {
   protected void configure() {
     bind(UserImpl.class).to(User.class).in(Singleton.class);
     bind(UserDAOImpl.class).to(UserDAO.class).in(Singleton.class);
+    bind(DalServiceImpl.class).to(DalService.class).in(Singleton.class);
   }
 }
