06065ca96b572dce78dbd712503ea9e9101769df
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-02-23 14:32:03 +0100

    add login dans UserUCC

--- src/main/java/be/vinci/pae/domain/ucc/UserUCC.java
+++ src/main/java/be/vinci/pae/domain/ucc/UserUCC.java
@@ -1,5 +1,30 @@
 package be.vinci.pae.domain.ucc;
 
+import be.vinci.pae.domain.dto.UserDTO;
+import be.vinci.pae.services.dao.UserDAO;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
 public class UserUCC {
+
+  private UserDAO userDAO;
+
+  /**
+   * Get a user associated with an email and check their password with the password entered.
+   *
+   * @param email      the user's email.
+   * @param motDePasse the user's hashed password.
+   * @return a UserDTO if existing user and correct password;.
+   */
+  public UserDTO login(String email, String motDePasse) {
+    UserDTO user;
+    user = userDAO.getOneUserByEmail(email);
+    if (user == null) {
+      throw new WebApplicationException(Response.status(Status.BAD_REQUEST)
+          .entity("Ressource not found").type("text/plain").build());
+    }
+    return user;
+  }
 
 }
