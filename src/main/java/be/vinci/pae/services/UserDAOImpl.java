c135bfbee4f975e22c1e74002880eff257d81e56
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-02-23 15:49:30 +0100

    refactor: getOneUserByEmail tmp

--- src/main/java/be/vinci/pae/services/UserDAOImpl.java
+++ src/main/java/be/vinci/pae/services/UserDAOImpl.java
@@ -2,11 +2,34 @@ package be.vinci.pae.services;
 
 import be.vinci.pae.domain.dto.UserDTO;
 import be.vinci.pae.services.dao.UserDAO;
+import be.vinci.pae.services.utils.DalServiceImpl;
+import jakarta.inject.Inject;
 
 public class UserDAOImpl implements UserDAO {
 
+  @Inject
+  private DalServiceImpl dalService;
+
   @Override
   public UserDTO getOneUserByEmail(String email) {
+    /*UserDTO userDTO;
+    String requestSql = """
+        SELECT id_utilisateur, nom, prenom, telephone, mot_de_passe, date_inscription, annee_academique, role
+        FROM utilisateurs
+        WHERE email = ?
+        """;
+    PreparedStatement ps = dalService.getPreparedStatement(requestSql);
+    try {
+      ps.setString(1, email);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+    try (ResultSet rs = ps.getResultSet()) {
+      userDTO.setEmail(rs.getString("email"));
+    }
+
+    return userDTO;*/
     return null;
   }
 }
