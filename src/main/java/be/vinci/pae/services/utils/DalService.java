d5496d148870271c4a6f095a447752beb2125ac0
Rayane Belbachir <rayane.belbachir@student.vinci.be>
2024-02-25 17:36:34 +0100

    refactor: add method getPreparedStatement()

--- src/main/java/be/vinci/pae/services/utils/DalService.java
+++ src/main/java/be/vinci/pae/services/utils/DalService.java
@@ -1,12 +1,11 @@
 package be.vinci.pae.services.utils;
 
-import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 /**
  * DalService Interface.
  */
 public interface DalService {
 
-  Connection conn = null;
-
+  PreparedStatement getPreparedStatement(String sql);
 }
