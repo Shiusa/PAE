6ac7e8f30a1b069f435cf42f8d0221df3550857e
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2024-03-01 14:24:25 +0100

    feature: add init.sql tables

--- init.sql
+++ init.sql
@@ -1,19 +1,50 @@
 DROP SCHEMA IF EXISTS proStage CASCADE;
 CREATE SCHEMA proStage;
 
-CREATE TABLE proStage.utilisateurs (
-    id_utilisateur SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
-    nom VARCHAR(100) NOT NULL,
-    prenom VARCHAR(100) NOT NULL,
-    telephone VARCHAR(20) NOT NULL,
-    mot_de_passe VARCHAR(60) NOT NULL,
-    date_inscription DATE NOT NULL,
-    annee_academique VARCHAR(9) NOT NULL,
-    role VARCHAR(13) NOT NULL
+CREATE TABLE proStage.users (
+                                user_id SERIAL PRIMARY KEY,
+                                email TEXT NOT NULL,
+                                lastname TEXT NOT NULL,
+                                firstname TEXT NOT NULL,
+                                phone_number TEXT NOT NULL,
+                                password TEXT NOT NULL,
+                                inscription_date DATE NOT NULL,
+                                school_year TEXT NOT NULL,
+                                role TEXT NOT NULL
 );
 
+CREATE TABLE proStage.companies (
+                                    company_id SERIAL PRIMARY KEY,
+                                    name TEXT,
+                                    designation TEXT,
+                                    address TEXT NOT NULL,
+                                    phone_number TEXT,
+                                    email TEXT,
+                                    is_blacklisted BOOLEAN NOT NULL,
+                                    blacklist_motivation TEXT
+);
+
+CREATE TABLE proStage.contacts (
+                                   contact_id SERIAL PRIMARY KEY,
+                                   company INTEGER NOT NULL REFERENCES proStage.companies (company_id),
+                                   student INTEGER NOT NULL REFERENCES proStage.users (user_id),
+                                   meeting TEXT,
+                                   contact_state TEXT NOT NULL,
+                                   reason_for_refusal TEXT,
+                                   school_year TEXT NOT NULL
+);
+
+
 -- MDP : 123
-INSERT INTO proStage.utilisateurs VALUES(DEFAULT, 'alice.dubois@vinci.be', 'Dubois', 'Alice', '+32471234567', '$2a$10$PO9RaYGTTyCohyiydzsyOuYMVZr7coRXYw8v4pmknkzGcKKLM/eAO', '2023-08-20', '2023-2024', 'administratif');
-INSERT INTO proStage.utilisateurs VALUES(DEFAULT, 'hugo.leroy@vinci.be', 'Leroy', 'Hugo', '+32478987654', '$2a$10$PO9RaYGTTyCohyiydzsyOuYMVZr7coRXYw8v4pmknkzGcKKLM/eAO', '2023-11-09', '2023-2024', 'professeur');
-INSERT INTO proStage.utilisateurs VALUES(DEFAULT, 'eleonore.martin@vinci.be', 'Martin', 'Éléonore', '+32485123456', '$2a$10$PO9RaYGTTyCohyiydzsyOuYMVZr7coRXYw8v4pmknkzGcKKLM/eAO', CURRENT_DATE, '2023-2024', 'etudiant');
+INSERT INTO proStage.users VALUES(DEFAULT, 'alice.dubois@vinci.be', 'Dubois', 'Alice', '+32471234567', '$2a$10$PO9RaYGTTyCohyiydzsyOuYMVZr7coRXYw8v4pmknkzGcKKLM/eAO', '2023-08-20', '2023-2024', 'administratif');
+INSERT INTO proStage.users VALUES(DEFAULT, 'hugo.leroy@vinci.be', 'Leroy', 'Hugo', '+32478987654', '$2a$10$PO9RaYGTTyCohyiydzsyOuYMVZr7coRXYw8v4pmknkzGcKKLM/eAO', '2023-11-09', '2023-2024', 'professeur');
+INSERT INTO proStage.users VALUES(DEFAULT, 'eleonore.martin@vinci.be', 'Martin', 'Éléonore', '+32485123456', '$2a$10$PO9RaYGTTyCohyiydzsyOuYMVZr7coRXYw8v4pmknkzGcKKLM/eAO', CURRENT_DATE, '2023-2024', 'etudiant');
+
+
+INSERT INTO proStage.companies VALUES(DEFAULT, 'DasInfo', NULL, 'Avenue des marais 80, Scharbeek 1500', '0412345678', 'das@auto.com', FALSE, NULL);
+INSERT INTO proStage.companies VALUES(DEFAULT, 'LSCustom', 'Bruxelles', 'Avenue des marais 80, Scharbeek 1500', '0412345678', 'das@auto.com', FALSE, NULL);
+INSERT INTO proStage.companies VALUES(DEFAULT, 'LSCustom', 'Anvers', 'Avenue des marais 80, Scharbeek 1500', '0412345678', 'das@auto.com', TRUE, 'Anvers est trop loin!');
+
+
+INSERT INTO proStage.contacts VALUES(DEFAULT, 1, 1, NULL, 'started', NULL, '2023-2024');
+INSERT INTO proStage.contacts VALUES(DEFAULT, 3, 2, 'remote', 'turned down', 'Student has not answered fast enough', '2023-2024');
