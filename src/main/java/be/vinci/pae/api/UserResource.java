143c847a04b185590117457d2deb173793fc288e
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-03-15 03:31:38 +0100

    add: register route with light input getters

--- src/main/java/be/vinci/pae/api/UserResource.java
+++ src/main/java/be/vinci/pae/api/UserResource.java
@@ -1,5 +1,6 @@
 package be.vinci.pae.api;
 
+import be.vinci.pae.domain.UserFactory;
 import be.vinci.pae.domain.dto.UserDTO;
 import be.vinci.pae.domain.ucc.UserUCC;
 import be.vinci.pae.utils.Config;
@@ -17,6 +18,8 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.sql.Date;
+import java.time.LocalDate;
 
 /**
  * UserResource class.
@@ -29,6 +32,8 @@ public class UserResource {
   private final ObjectMapper jsonMapper = new ObjectMapper();
   @Inject
   private UserUCC userUCC;
+  @Inject
+  private UserFactory userFactory;
 
   /**
    * Login route.
@@ -69,6 +74,55 @@ public class UserResource {
       System.out.println("Error while creating token");
       throw new WebApplicationException("error while creating token", Response.Status.UNAUTHORIZED);
     }
+  }
+
+  /**
+   * Register route.
+   *
+   * @param json object containing email, lastname, firstname, phone number, password, role.
+   * @return a User.
+   */
+  @POST
+  @Path("register")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public ObjectNode register(JsonNode json) {
+
+    String email = json.get("email").asText();
+    String lastname = json.get("lastname").asText();
+    String firstname = json.get("firstname").asText();
+    String phoneNumber = json.get("phoneNumber").asText();
+    String password = json.get("password").asText();
+    LocalDate localDate = LocalDate.now();
+    Date registrationDate = Date.valueOf(localDate);
+    int monthValue = localDate.getMonthValue();
+    String schoolYear;
+    if (monthValue >= 9) {
+      schoolYear =
+          String.valueOf(localDate.getYear()) + "-" + String.valueOf(localDate.plusYears(1));
+    } else {
+      schoolYear =
+          String.valueOf(localDate.minusYears(1)) + "-" + String.valueOf(localDate.getYear());
+    }
+    String role = json.get("role").asText();
+
+    UserDTO user = userFactory.getUserDTO();
+    user.setEmail(email);
+    user.setLastname(lastname);
+    user.setFirstname(firstname);
+    user.setPhoneNumber(phoneNumber);
+    user.setPassword(password);
+    user.setRegistrationDate(registrationDate);
+    user.setSchoolYear(schoolYear);
+    user.setRole(role);
+
+    UserDTO registeredUser = userUCC.register(user);
+
+    ObjectNode publicUser = jsonMapper.createObjectNode()
+        .putPOJO("user", registeredUser);
+
+    return publicUser;
+
   }
 }
 
