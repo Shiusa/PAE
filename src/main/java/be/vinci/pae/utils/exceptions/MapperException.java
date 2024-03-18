7b8b7f2a897e075e4b882fe0d750d2e00333a590
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2024-03-18 09:10:04 +0100

    feat: mapper exception

---
+++ src/main/java/be/vinci/pae/utils/exceptions/MapperException.java
@@ -0,0 +1,27 @@
+package be.vinci.pae.utils.exceptions;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class MapperException implements ExceptionMapper<Throwable> {
+
+  @Override
+  public Response toResponse(Throwable exception) {
+    return Response.status(getCode(exception)).build();
+  }
+
+  /**
+   * Get the code of the web exception.
+   *
+   * @param e the exception.
+   * @return the code.
+   */
+  private int getCode(Throwable e) {
+    if (e instanceof DuplicateException) {
+      return 409;
+    }
+    return 500;
+  }
+}
