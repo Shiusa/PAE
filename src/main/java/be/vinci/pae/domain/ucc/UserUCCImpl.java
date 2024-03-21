b9bb7dbace95fd7e696b9610766b28b1f28373b2
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-03-21 16:38:00 +0100

    add: login transaction

--- src/main/java/be/vinci/pae/domain/ucc/UserUCCImpl.java
+++ src/main/java/be/vinci/pae/domain/ucc/UserUCCImpl.java
@@ -3,6 +3,7 @@ package be.vinci.pae.domain.ucc;
 import be.vinci.pae.domain.User;
 import be.vinci.pae.domain.dto.UserDTO;
 import be.vinci.pae.services.dao.UserDAO;
+import be.vinci.pae.services.utils.DalServices;
 import jakarta.inject.Inject;
 
 /**
@@ -12,39 +13,44 @@ public class UserUCCImpl implements UserUCC {
 
   @Inject
   private UserDAO userDAO;
+  @Inject
+  private DalServices dalServices;
 
   /**
    * Get a user associated with an email and check their password with the password entered.
    *
-   * @param email      the user's email.
+   * @param email    the user's email.
    * @param password the user's hashed password.
    * @return a UserDTO if existing user and correct password;.
    */
   @Override
   public UserDTO login(String email, String password) {
+
+    dalServices.startTransaction();
     UserDTO userDTOFound = userDAO.getOneUserByEmail(email);
 
     User user = (User) userDTOFound;
 
     if (user == null || !user.checkPassword(password)) {
+      dalServices.rollback();
       return null;
     }
 
-    userDTOFound.setPassword(null);
+    dalServices.commit();
     return userDTOFound;
   }
 
   /**
    * Register a user.
    *
-   * @param user  user to register.
+   * @param user user to register.
    * @return a UserDTO of registered user, null otherwise.
    */
   @Override
   public UserDTO register(UserDTO user) {
 
     UserDTO existingUser = userDAO.getOneUserByEmail(user.getEmail());
-    if (existingUser!=null) {
+    if (existingUser != null) {
       return null;
     }
 
@@ -52,7 +58,7 @@ public class UserUCCImpl implements UserUCC {
     userHashPwd.hashPassword();
 
     UserDTO registeredUser = userDAO.addOneUser((UserDTO) userHashPwd);
-    if (registeredUser==null){
+    if (registeredUser == null) {
       return null;
     }
     return registeredUser;
