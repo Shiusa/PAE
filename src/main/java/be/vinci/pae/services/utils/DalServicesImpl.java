915e72b32713541119b03727c755ca5f15188b7e
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-03-22 03:42:57 +0100

    refactor: Connection with BasicDataSource

--- src/main/java/be/vinci/pae/services/utils/DalServicesImpl.java
+++ src/main/java/be/vinci/pae/services/utils/DalServicesImpl.java
@@ -2,9 +2,9 @@ package be.vinci.pae.services.utils;
 
 import be.vinci.pae.utils.Config;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  * Implementation of DalService.
@@ -12,31 +12,54 @@ import java.sql.SQLException;
 public class DalServicesImpl implements DalServices, DalBackendServices {
 
   //private Connection connection = null;
+
   private static final ThreadLocal<Connection> connectionThreadLocal = ThreadLocal.withInitial(
       () -> {
         try {
-          return DriverManager.getConnection(Config.getProperty("postgresUrl"),
-              Config.getProperty("postgresUser"),
-              Config.getProperty("postgresPassword"));
-        } catch (SQLException e) {
-          throw new RuntimeException(e);
+          return getDataSource().getConnection();
+        } catch (SQLException throwables) {
+          throwables.printStackTrace();
         }
+        return null;
       });
 
   /**
-   * Create connection to database.
+   * Create connection pool.
+   *
+   * @return BasicDataSource of connection.
    */
-  /*public DalServicesImpl() {
+  private static BasicDataSource getDataSource() {
+    BasicDataSource dataSource = new BasicDataSource();
     try {
-      this.connection = DriverManager.getConnection(Config.getProperty("postgresUrl"),
-          Config.getProperty("postgresUser"),
-          Config.getProperty("postgresPassword"));
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+      dataSource.setUrl(Config.getProperty("postgresUrl"));
+      dataSource.setUsername(Config.getProperty("postgresUser"));
+      dataSource.setPassword(Config.getProperty("postgresPassword"));
+      dataSource.setMinIdle(2);
+      dataSource.setMaxIdle(10);
+      dataSource.setMaxOpenPreparedStatements(100);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-  }*/
-  private static Connection getConnection() {
-    return connectionThreadLocal.get();
+    return dataSource;
+  }
+
+  /**
+   * Get a connection.
+   *
+   * @return Connection.
+   */
+  private Connection getConnection() {
+    Connection connection = connectionThreadLocal.get();
+    try {
+      if (connection == null || connection.isClosed()) {
+        connection = getDataSource().getConnection();
+        connectionThreadLocal.set(connection);
+        return connectionThreadLocal.get();
+      }
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+    return connection;
   }
 
   /**
@@ -81,8 +104,8 @@ public class DalServicesImpl implements DalServices, DalBackendServices {
     } finally {
       try {
         connection.close();
-      } catch (SQLException e) {
-        throw new RuntimeException(e);
+      } catch (SQLException throwables) {
+        throwables.printStackTrace();
       }
       connectionThreadLocal.remove();
     }
@@ -101,8 +124,8 @@ public class DalServicesImpl implements DalServices, DalBackendServices {
     } finally {
       try {
         connection.close();
-      } catch (SQLException e) {
-        throw new RuntimeException(e);
+      } catch (SQLException throwables) {
+        throwables.printStackTrace();
       }
       connectionThreadLocal.remove();
     }
