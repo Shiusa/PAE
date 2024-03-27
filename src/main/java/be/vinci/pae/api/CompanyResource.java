0625bc076bef53e1c43532694b34f3a72f98ef4a
Nguyen Kyle <trong.nguyen@student.vinci.be>
2024-03-27 23:01:23 +0100

    feat: companyResource

---
+++ src/main/java/be/vinci/pae/api/CompanyResource.java
@@ -0,0 +1,42 @@
+package be.vinci.pae.api;
+
+import be.vinci.pae.api.filters.Authorize;
+import be.vinci.pae.domain.dto.CompanyDTO;
+import be.vinci.pae.domain.ucc.CompanyUCC;
+import be.vinci.pae.utils.Logs;
+import be.vinci.pae.utils.exceptions.FatalException;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+import org.apache.logging.log4j.Level;
+
+public class CompanyResource {
+
+  @Inject
+  private CompanyUCC companyUCC;
+
+  /**
+   * Get all companies.
+   *
+   * @return a list containing all the companies.
+   */
+  @GET
+  @Path("all")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public List<CompanyDTO> getAll() {
+    Logs.log(Level.INFO, "CompanyResource (getAll) : entrance");
+    List<CompanyDTO> companyDTOList;
+    try {
+      companyDTOList = companyUCC.getAllCompanies();
+    } catch (FatalException e) {
+      throw e;
+    }
+    Logs.log(Level.INFO, "CompanyResource (getAll) : success!");
+    return companyDTOList;
+  }
+
+}
