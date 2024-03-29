5bcceec38fbe59efde5cb9842c38674459a2c46b
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2024-03-29 17:50:41 +0100

    refactor: changing exception mapper to log errors

--- src/main/java/be/vinci/pae/utils/WebExceptionMapper.java
+++ src/main/java/be/vinci/pae/utils/WebExceptionMapper.java
@@ -11,6 +11,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import org.apache.logging.log4j.Level;
 
 /**
  * WebExceptionMapper class.
@@ -20,38 +21,46 @@ public class WebExceptionMapper implements ExceptionMapper<Throwable> {
   @Override
   public Response toResponse(Throwable exception) {
     if (exception instanceof FatalException) {
+      Logs.log(Level.ERROR, "Error : " + exception);
       return Response.status(Status.INTERNAL_SERVER_ERROR)
           .entity(exception.getMessage())
           .build();
     }
     if (exception instanceof InvalidRequestException) {
+      Logs.log(Level.ERROR, "Error : " + exception);
       return Response.status(Status.BAD_REQUEST)
           .entity(exception.getMessage())
           .build();
     }
     if (exception instanceof ResourceNotFoundException) {
+      Logs.log(Level.ERROR, "Error : " + exception);
       return Response.status(Status.NOT_FOUND)
           .entity(exception.getMessage())
           .build();
     }
     if (exception instanceof UnauthorizedAccessException) {
+      Logs.log(Level.ERROR, "Error : " + exception);
       return Response.status(Status.UNAUTHORIZED)
           .entity(exception.getMessage())
           .build();
     }
     if (exception instanceof DuplicateException) {
+      Logs.log(Level.ERROR, "Error : " + exception);
       return Response.status(Status.CONFLICT)
           .entity(exception.getMessage())
           .build();
     }
     if (exception instanceof NotAllowedException) {
+      Logs.log(Level.ERROR, "Error : " + exception);
       return Response.status(Status.FORBIDDEN)
           .entity(exception.getMessage())
           .build();
     }
     if (exception instanceof WebApplicationException) {
+      Logs.log(Level.ERROR, "Error : " + exception);
       return ((WebApplicationException) exception).getResponse();
     }
+    Logs.log(Level.ERROR, "Error : " + exception);
     return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
         .entity(exception.getMessage())
         .type(MediaType.TEXT_PLAIN)
