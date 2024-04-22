1b4e12ca56cc0999d61af172e75a823c552ec87f
brian villanueva <brian.villanueva@student.vinci.be>
2024-04-22 11:12:12 +0200

    feat: add test

--- src/test/java/be/vinci/pae/InternshipUCCImplTest.java
+++ src/test/java/be/vinci/pae/InternshipUCCImplTest.java
@@ -115,6 +115,13 @@ public class InternshipUCCImplTest {
   }
 
   @Test
+  @DisplayName("Test editProject correctly")
+  public void testEditProjectCorrectly() {
+    Mockito.when(internshipDAOMock.editProject("test", 1, 1)).thenReturn(internshipDTO);
+    assertNotNull(internshipUCC.editProject("test", 1, 1));
+  }
+
+  @Test
   @DisplayName("Test crash transaction")
   public void testCrashTransaction() {
     Mockito.doThrow(new FatalException(new RuntimeException()))
@@ -125,8 +132,10 @@ public class InternshipUCCImplTest {
         }),
         () -> assertThrows(FatalException.class, () -> {
           internshipUCC.getOneById(1, 1);
-        })
-    );
+        }),
+        () -> assertThrows(FatalException.class, () -> {
+          internshipUCC.editProject("test", 1, 1);
+        }));
   }
 }
 
